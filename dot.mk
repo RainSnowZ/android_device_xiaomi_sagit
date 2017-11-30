1cab6b7a085e3016250aada0fe8afa8e37aa00b9
Alkiyanya <1376106928@qq.com>
2017-12-01 00:05:29 +0800

    Dot-ify

--- dot.mk
+++ dot.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Dot stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sagit
+PRODUCT_NAME := dot_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
@@ -13,3 +13,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="Xiaomi/sagit/sagit:7.1.1/NMF26X/7.11.23:user/release-keys"
+
+# DotExports
+
+WITH_MAGISK := true
+DOT_BUILDTYPE := UNOFFICIAL
